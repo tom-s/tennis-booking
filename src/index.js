7afb6a41663f910d3769e9de26e352ac62e876cf
tom-s <thom.schell@gmail.com>
2016-12-13 21:33:35 +0100

    select day

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import NightmareFactory from 'nightmare'
 
 import { LOGIN_URL, USERNAME, PWD } from '../config'
 
+const pad = (num, size=2) => {
+  let s = num +'';
+  while (s.length < size) s = '0' + s
+  return s
+}
+
+const extractDate = (dateStr='', separator = '/') => {
+   const data = dateStr.split(separator)
+   return {
+     day: pad(data[0]),
+     month: pad(data[1]),
+     year: pad(data[2])
+   }
+}
+
+// return `${pad(data[0])}${pad(data[1])}${pad(data[2])}`
+
 //
 // Start the prompt
 //
@@ -11,20 +28,22 @@ prompt.start()
 //
 // Get two properties from the user: username and email
 //
-prompt.get(['date'], (err, result) => {
+prompt.get(['date'], (err, { date }) => {
   //
   // Log the results.
   //
   console.log('Command-line input received:')
-  console.log('  date: ' + result.date)
+  console.log('  date: ' + date)
 
-  runBooking()
+  const dateObj = extractDate(date)
+
+  runBooking(dateObj)
 })
 
 
 
 /* Run casper scraper */
-const runBooking = () => {
+const runBooking = (date) => {
   const nightmare = NightmareFactory({ show: true })
   nightmare
     .goto(LOGIN_URL)
@@ -32,10 +51,11 @@ const runBooking = () => {
     .type('form[name=membreLoginForm] [name=password]', PWD)
     .click('form[name=membreLoginForm] input[name=buttonConnecter]')
     .wait('form[name=membreIdentiteForm]')
-    .goto('https://adoc.app.fft.fr/adoc/tableauJourJoueur.do?method=readJoueur')
+    .click('a[title="Tableaux par jour"]')
     .wait('#tableauCourt')
-    //.click('a#fd-but-date')
-    //.click('.cd-20161205')
+    .click('a#fd-but-date')
+    .click(`.cd-${date.year}${date.month}${date.day}`)
+    .wait('#fsdfds')
     .end()
     .then(function (result) {
       console.log("Booking done !", result)
