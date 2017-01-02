51b7f0f83f4b1f5041ab172b158ea084854d9d9e
tom-s <thom.schell@gmail.com>
2017-01-02 23:56:29 +0100

    fixes

--- backend/email.js
+++ backend/email.js
@@ -9,22 +9,21 @@ export const EMAIL_TEMPLATES = {
   SUCCESS: {
     BOOKING_PLANNED: {
       subject: 'Booking planned for %date on court %court',
-      text: 'The schedule job will run on %executionDate, I will not fail you!'
+      text: 'The scheduled job will run on %executionDate, I will not fail you.'
     },
     BOOKING_DONE: {
       subject: 'Booking done for %date on court %court',
-      text: 'I have not failed you, its tennis time!'
+      text: "I have not failed you, it's tennis time..."
     }
   },  
   ERROR: {
-    subject: 'Booking have failed for %date on court %court, perhaps the court is not available ?',
-    text: 'I have failed you ! (but also, I really think the court is not available !)'
+    subject: 'Booking has failed for %date on court %court',
+    text: "I have failed you. I'm pretty sure the court is not available."
   }
 }
 
 export const sendEmail = ({subject='test email', text='test email', to='thom.schell@gmail.com', cc='hester.borren@gmail.com'}) => {
   return new Promise((resolve, reject) => {
-    resolve()
     const message = {
       from: 'tennis-booking@thomster.ddns.net',
       to,
--- backend/nightmare.js
+++ backend/nightmare.js
@@ -4,9 +4,15 @@ import { LOGIN_URL, USERNAME, PWD, PLAYERS } from '../../config'
 /* Run nightmare scraper */
 export const runBooking = ({dateObj:date, startTime, endTime, court}) => {
   const dateStr = date.day + '/' + date.month + '/' + date.year
+  const courtId = court == 1
+    ? 21133
+    : 23683
+
   return new Promise((resolve, reject) => {
+    let idCreneau
     const nightmare = NightmareFactory({
-      show: false,
+      //show: true,
+      //openDevTools: true,
       typeInterval: 20,
       pollInterval: 50 //in ms
     })
@@ -20,14 +26,29 @@ export const runBooking = ({dateObj:date, startTime, endTime, court}) => {
       .wait('a#fd-but-date')
       .evaluate((dateStr) => {
         window.moveToThisDate(document.tableauJourForm, $('date'), dateStr)
-        return true
       }, dateStr)
-      .wait(500) // just in case
-      .wait(`div[title*="${startTime}h00 à"]`)
-      .click(`#scrollDonneesTableau>div .colonneCourtJour:nth-child(${court}) div[title*="${startTime}h00 à"]`)
-      .wait('form#reservationPonctuelleJoueurForm')
+      .wait(1000) // give some time fot the page to reload -> this is dirty but i haven't found any better
+      .evaluate((dateStr, startTime,courtId,  done) => {
+          // It's a bit dodgy code, but at least it works in electron (no need to be compiled)
+          var boxes = document.querySelectorAll('.donnee')
+          let ids = []
+          for (var i = 0; i < boxes.length; i++) {
+            var box = boxes[i]
+            var boxId  = box.getAttribute('id')
+            if(boxId) {
+              ids = ids.filter(id => id !== boxId)
+              ids.push(boxId)
+            }
+          }
+          var id = ids.find(id => id.indexOf(courtId) !== -1)
+          var idCreneau = parseInt(id.split('_')[0].replace('creneau', ''), 10)
+          window.ajoutReservation(idCreneau,`${startTime}:00`, dateStr)
+          done(null, idCreneau)
+      }, dateStr, startTime,courtId)
       .evaluate(() => {
         // Make hidden inputs visible so that we can modify their values
+        document.querySelector('#identifiantCreneau').type = 'text'
+        document.querySelector('#date').type = 'text'
         document.querySelector('#identifiantMembreDeux_value').type = 'text'
         document.querySelector('#nomMembre2_value').type = 'text'
         return true
